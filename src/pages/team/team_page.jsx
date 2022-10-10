e21d2c763f28e85886caaedc2469218e99077348
Fernando Melgar <fernandom.melgar@gmail.com>
2022-10-10 10:39:11 -0500

    Add Correct photos url for team page

--- src/pages/team/team_page.jsx
+++ src/pages/team/team_page.jsx
@@ -2,195 +2,195 @@ import MemberPhoto from "./components/member_photo";
 
 const data = [
   {
-    name: "Claudia A. Garcia",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Claudia A. Garcia",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/claudia-centrocentro.jpg",
   },
   {
-    name: "Yaroslav A. Volkov",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Yaroslav A. Volkov",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/yaros-centrocentro.jpg",
   },
   {
-    name: "Brenda Jimenez",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Brenda Jimenez",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/brenda-centrocentro.jpg",
   },
   {
-    name: "Jose Antonio Munóz",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Jose Antonio Munóz",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/joseantonio-centrocentro.jpg",
   },
   {
-    name: "Sergio García",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Sergio García",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/sergio-centrocentro.jpg",
   },
   {
-    name: "Andrea de los santos",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Andrea de los santos",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/andrea-centrocentro.jpg",
   },
   {
-    name: "Lizeth Cameras",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Lizeth Cameras",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/lizeth-centrocentro.jpg",
   },
   {
-    name: "Audrey Hevia",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Audrey Hevia",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/audrey-centrocentro.jpg",
   },
   {
-    name: "Melchor A. Soto",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Melchor A. Soto",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/melchor-centrocentro.jpg",
   },
   {
-    name: "Ana Sofía Centeno",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Ana Sofía Centeno",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/anasofia-centrocentro.jpg",
   },
   {
-    name: "Maren I. Marín",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Maren I. Marín",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/maren-centrocentro.jpg",
   },
   {
-    name: "Amanda Spínola",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Amanda Spínola",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/amanda-centrocentro.jpg",
   },
   {
-    name: "Ariadna Alonso",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Ariadna Alonso",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/ariadna-centrocentro.jpg",
   },
   {
-    name: "Amiel J. Rosete",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Amiel J. Rosete",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/amiel-centrocentro.jpg",
   },
   {
-    name: "Valeria Torres",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Valeria Torres",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/valeria-centrocentro.jpg",
   },
   {
-    name: "Isabel Herández",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Isabel Herández",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/isa-centrocentro.jpg",
   },
   {
-    name: "Hector A. Murro",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Hector A. Murro",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/hector-centrocentro.jpg",
   },
   {
-    name: "Humberto Carrillo",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Humberto Carrillo",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/humberto-centrocentro.jpg",
   },
   {
-    name: "Saúl Higadera",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Saúl Higadera",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/saul-centrocentro.jpg",
   },
   {
-    name: "Regina Rivas",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Regina Rivas",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/regina-centrocentro.jpg",
   },
   {
-    name: "Jennifer Rivera",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Jennifer Rivera",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/jennifer-centrocentro.jpg",
   },
   {
-    name: "Ximena Quintero",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Ximena Quintero",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/ximena-centrocentro.jpg",
   },
   {
-    name: "Rebeca Patiño",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Rebeca Patiño",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/rebeca-centrocentro.jpg",
   },
   {
-    name: "Ana Paula Martínez",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Ana Paula Martínez",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/anapaula-centrocentro.jpg",
+  },
+  {   
+      name: "José Gerardo Altamirano",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/jerry-centrocentro.jpg",
   },
   {
-    name: "José Gerardo Altamirano",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Salvador Chavez",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/salvador-centrocentro.jpg",
   },
   {
-    name: "Salvador Chavez",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Mirelle Culebro",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/mirelle-centrocentro.jpg",
   },
   {
-    name: "Mirelle Culebro",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Arturo Ortíz",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/arturo-centrocentro.jpg",
+  }, {
+      name: "Rodrigo Labastida",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/rodrigo-centrocentro.jpg",
   },
-  {
-    name: "Arturo Ortíz",
-    imgUrl: "https://placeimg.com/192/192/people",
-  },
-  {
-    name: "Rodrigo Labastida",
-    imgUrl: "https://placeimg.com/192/192/people",
-  },
-];
+]
 
 const visualsTeam = [
   {
-    name: "Sofía Azamar",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Sofía Azamar",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/sofi-centrocentro.jpg",
   },
   {
-    name: "Selma M. Martínez",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Selma M. Martínez",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/selma-centrocentro.jpg",
   },
   {
-    name: "Itzel Ortega",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Itzel Ortega",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/itzel-centrocentro.jpg",
   },
   {
-    name: "Minerva Zamudio",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Minerva Zamudio",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/minerva-centrocentro.jpg",
   },
   {
-    name: "Sara S. Hernández",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Sara S. Hernández",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/sara-centrocentro.jpg",
   },
   {
-    name: "Dino Hernández",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Dino Hernández",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/dino-centrocentro.jpg",
   },
   {
-    name: "Zyanya Rojas",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Zyanya Rojas",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/zyan-centrocentro.jpg",
   },
   {
-    name: "Valeria G. Zúñiga",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Valeria G. Zúñiga",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/valeria-centrocentro.jpg",
   },
   {
-    name: "Damiela Mejía",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Daniela Mejía",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/daniela-centrocentro.jpg",
   },
-];
+]
 
 const professors = [
   {
-    name: "Dulce V. Mello",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Dulce V. Mello",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/ddulce-centrocentro.jpg",
   },
   {
-    name: "Cesar García",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Cesar García",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/cesar-centrocentro.jpg",
   },
   {
-    name: "Roberto Delgado",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Roberto Delgado",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/roberto-centrocentro.jpg",
   },
   {
-    name: "Elías Octavio Gomez",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Elías Octavio Gomez",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/elias-centrocentro.jpg",
   },
   {
-    name: "Carlos E. Gomez",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Carlos E. Gomez",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/carlos-centrocentro.jpg",
   },
   {
-    name: "Ramon E. Rivero",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Ramon E. Rivero",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/ramon-centrocentro.jpg",
   },
   {
-    name: "Aurora Antonio P.",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Aurora Antonio P.",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/aurora-centrocentro.jpg",
   },
   {
-    name: "Ana Laura Torres",
-    imgUrl: "https://placeimg.com/192/192/people",
+      name: "Ana Laura Torres",
+      imgUrl: "https://static.igem.wiki/teams/4260/wiki/analaura-centrocentro.jpg",
   },
+
 ];
 
 export default function TeamPage() {
