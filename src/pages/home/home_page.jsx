17f02887b2729537f7cd952f60ca203b4afb48b0
Fernando Melgar <fernandom.melgar@gmail.com>
2022-10-10 11:12:19 -0500

    Delete sections to solve bg imports

--- src/pages/home/home_page.jsx
+++ src/pages/home/home_page.jsx
@@ -39,9 +39,6 @@ export default function HomePage() {
           </div>
         </div>
       </PageWithBackgroundVideo>
-      <EndocrineDisruptiveSection />
-      <ProblemExplanationSection />
-      <OurMissionSection />
     </div>
   );
 }
