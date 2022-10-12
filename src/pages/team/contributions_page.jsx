78db57953218a11a818b2d591348708b020bcfea
Fernando Melgar <fernandom.melgar@gmail.com>
2022-10-12 01:51:47 -0500

    Agregar imagen de fondo a collaborations

--- src/pages/team/contributions_page.jsx
+++ src/pages/team/contributions_page.jsx
@@ -1,14 +1,34 @@
 export default function ContributionsPage() {
-    return (
-      <div
-        className="pt-16"
-        style={{
-          backgroundImage: `url(https://static.igem.wiki/teams/4260/wiki/imagen/team/contributions/0-fondos-animados-gifs/0-fondo-animado-1-default.gif)`,
-          minHeight: "100vh",
-        }}>
-        <div className="p-10 mt-10 max-w-5xl m-auto text-white rounded-md" style={{backgroundColor: 'rgba(66, 138, 245,.7)'}}>
-          <h1 className="text-6xl text-center text-white font-bold">
-            CONTRIBUTIONS
-          </h1>
+  return (
+    <div className="hero">
+      <img
+        src="https://static.igem.wiki/teams/4260/wiki/imagen/team/contributions/0-fondos-animados-gifs/0-fondo-animado-1-default.gif"
+        alt="background"
+        style={{ width: "100vw" }}
+      />
+      <div className="h-full w-full py-20 text-white">
+        <h1 className="text-7xl cano text-center text-white">CONTRIBUTIONS</h1>
+        <div
+          className="max-w-3xl m-auto"
+          style={{ backgroundColor: "rgba(245, 233, 98,.9)" }}
+        >
+          <p className="p">
+            This year iGEM Tec CEM team worked towards the detection and
+            degradation of EDCs found in the residual water used to irrigate
+            agricultural crops in Mexico, in order to regulate their
+            accumulation in plant tissues, hence reducing the consumption of
+            EDCs through food.
+          </p>
+          <img
+            src="https://static.igem.wiki/teams/4260/wiki/contributions/figura1dise-ofinalbioseonsor-contributions.jpeg"
+            alt=" Figura1"
+            className="figure"
+            style={{ maxWidth: 300 }}
+          />
+          <p className="figure_desc">soy un pie de pagina</p>
+          
         </div>
-        </div>);}
+      </div>
+    </div>
+  );
+}
